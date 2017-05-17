fc27c5732ec78cd2c34244ae2d6eae752652a5ab
Brian <brian.w.projects@gmail.com>
2017-05-17 23:15:43 +0000

    email testing

--- app/email.py
+++ app/email.py
@@ -2,10 +2,24 @@ from flask import current_app, render_template
 from . import mail
 from flask_mail import Message
 from .tasks import send_async_email
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
-    msg = Message('Confirm Account',
-                  sender=app.config['MAIL_USERNAME'], recipients=[to])
-    msg.body = render_template(template + '.txt', **kwargs)
-    send_async_email(app, msg)
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email(app.config['MAIL_USERNAME'])
+    to_email = Email(to)
+    subject = "Confirm Account"
+    content = Content(render_template(template + '.txt', **kwargs))
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    
+    
+    
+    # app = current_app._get_current_object()
+    # msg = Message('Confirm Account',
+    #               sender=app.config['MAIL_USERNAME'], recipients=[to])
+    # msg.body = render_template(template + '.txt', **kwargs)
+    # send_async_email(app, msg)
