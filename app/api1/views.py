d11eeeea360b248a5a4d61134d51792a21d6f55b
Brian <brian.w.projects@gmail.com>
2017-05-23 00:15:36 +0000

    changing search api

--- app/api1/views.py
+++ app/api1/views.py
@@ -334,17 +334,17 @@ def get_search_recs(page = 1):
     display_recs = Recommendation.query\
         .join(User)\
         .filter(Recommendation.verification > 0)
-    if request.get_json(silent=True):
-        if request.json.get('term') is not None:
-            display_recs = display_recs\
-                .filter(Recommendation.title.contains(request.json.get('term')))
-        if request.json.get('user') is not None:
-            display_recs = display_recs\
-                .filter(User.username.contains(request.json.get('user')))
-        if request.json.get('date') is not None:
-            date =  datetime.strptime(request.json.get('date'), '%m/%d/%Y') + timedelta(days=1)
-            display_recs = display_recs\
-                .filter(Recommendation.timestamp<=date)
+    # if request.get_json(silent=True):
+    if request.args.get('term') is not None:
+        display_recs = display_recs\
+            .filter(Recommendation.title.contains(request.json.get('term')))
+    if request.args.get('user') is not None:
+        display_recs = display_recs\
+            .filter(User.username.contains(request.json.get('user')))
+    if request.args.get('date') is not None:
+        date =  datetime.strptime(request.json.get('date'), '%m/%d/%Y') + timedelta(days=1)
+        display_recs = display_recs\
+            .filter(Recommendation.timestamp<=date)
     display_recs = display_recs\
         .order_by(desc(Recommendation.timestamp))\
         .paginate(page, per_page=g.current_user.display, error_out=False)
